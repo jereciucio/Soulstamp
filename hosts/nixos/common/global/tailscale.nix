285bc0a5dd6745ba613da11defa2c9a139e83b63
Jeremías Agustín Martinez Ciucio <jeremias@jeremiasmartinez.com>
2026-09-06 00:13:39 -0300

    feat: use sops for tailscale login

--- hosts/nixos/common/global/tailscale.nix
+++ hosts/nixos/common/global/tailscale.nix
@@ -1,10 +1,21 @@
-{lib, ...}: {
+{lib, config, ...}: {
   services.tailscale = {
     enable = true;
 
     # Receive routes by default, overridable for possible future servers
     useRoutingFeatures = lib.mkDefault "client";
+
+    # Autologin when setting up a new host
+    authKeyFile = config.sops.secrets.tailscale_authkey.path;
+    
   };
+
+  sops.secrets = {
+    tailscale_authkey = {
+      sopsFile = ../../../secrets.yaml;
+    };
+  };
+  
   # Easier NAT traversal when possible
   networking.firewall.allowedUDPPorts = [41641];
 
