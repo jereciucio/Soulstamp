d03abe7aa104ee9327d962b339c750bdf8a2607b
Jeremías Agustín Martinez Ciucio <jeremias@jeremiasmartinez.com>
2026-09-06 13:52:39 -0300

    fix: override nextcloud-client build to fix crash on GNOME

--- home/jeremiasmc/features/desktop/common/nextcloud.nix
+++ home/jeremiasmc/features/desktop/common/nextcloud.nix
@@ -1,6 +1,15 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  # Override package build to fix nextcloud-client crashing
+  # on GNOME. See https://github.com/NixOS/nixpkgs/issues/463016
+  nextcloud-client = pkgs.nextcloud-client.overrideAttrs (old: {
+    qtWrapperArgs =
+      (old.qtWrapperArgs or [])
+      ++ ["--prefix XDG_DATA_DIRS : ${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}"];
+  });
+in {
   services.nextcloud-client = {
     enable = true;
     startInBackground = true;
+    package = nextcloud-client;
   };
 }
