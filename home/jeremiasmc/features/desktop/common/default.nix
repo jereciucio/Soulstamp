63ec1461327a5d72aa99d839ecf8df770dd6ee14
Jeremías Agustín Martinez Ciucio <jeremias@jeremiasmartinez.com>
2026-09-06 10:47:35 -0300

    feat: add nextcloud

--- home/jeremiasmc/features/desktop/common/default.nix
+++ home/jeremiasmc/features/desktop/common/default.nix
@@ -2,5 +2,12 @@
   imports = [
     ./zen-browser.nix
     ./feishin.nix
+    ./nextcloud.nix
   ];
+
+  home.persistence = {
+    "/persist".directories = [
+      ".config/Nextcloud"
+    ];
+  };
 }
